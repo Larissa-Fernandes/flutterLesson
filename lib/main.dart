c7bbb84fdaeb7d015947fef334042a47c28e14e1
Larissa Fernandes <larissa.afernandes@outlook.com>
2021-09-07 13:03:42 -0300

    Create: StatefulWidget | same result as before

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,34 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {   /*  StatelessWidget: os widgets nao mudam suas caracteristicas. */
-  @override                             /*  Sobrescreve um método (build()), por ser uma classe abstrata
-                                            e nós devemos determinar seu comportamento */
-  Widget build(BuildContext context) {  /*  BuildContext: Localiza cada widget */
-    final wordPair = WordPair.random();
-    return MaterialApp(                 /*  ver Material design */
+class MyApp extends StatelessWidget {   // StatelessWidget: os widgets nao mudam suas caracteristicas.
+  @override                             // Sobrescreve um método (build()), por ser uma classe abstrata e nós devemos determinar seu comportamento
+  Widget build(BuildContext context) {  //  BuildContext: Localiza cada widget
+    return MaterialApp(                 //  ver Material design
       title: 'Welcome to Flutter',
-      home: Scaffold(                   /*  Scaffold: permite montar a estrutura básica de uma tela */
+      home: Scaffold(                   //  Scaffold: permite montar a estrutura básica de uma tela
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase), /* asPascalCase == UpperCamelCase*/
+          child: RandomWords(), 
         ),
       ),
     );
+  }
+}
+
+//StatefulWidget (stful)
+
+class RandomWords extends StatefulWidget {
+  @override 
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {  // por padrão, States são prefixados com _
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);   // asPascalCase == UpperCamelCase
   }
 }
